3d4e2bba5509de38038eb34e419ea0b38ca511e3
Can SEVER <can-svr@hotmail.com>
2022-04-17 19:04:03 +0300

    Unit test eklendi.

---
+++ UnitTestWebApi/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestWebApi")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestWebApi")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("539c96c3-c081-4013-bc17-3d4d2f11e7de")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestWebApi/UnitTest.cs
@@ -0,0 +1,43 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.Controllers;
+
+namespace UnitTestWebApi
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void GetUser()
+        {
+            IList<User> userList = new List<User>();
+            userList = new UserController().GetUserList();
+            Assert.IsNotNull(userList);
+        }
+
+        [TestMethod]
+        public void AddUser()
+        {
+            var result = "";
+            User user = new User();
+            user = new User() { USER_NAME = "Can", USER_SURNAME = "Sever", USER_MAIL = "can.sever@bilgiyon.com.tr" };
+            result = new UserController().AddUser(user);
+            Assert.AreEqual(user.USER_NAME +" Kullanıcısı için Ekleme Başarılı!", result);
+        }
+
+        [TestMethod]
+        public void UpdateUser()
+        {
+            string result = "";
+            User user = new User();
+            user = new User() { USER_CODE = 1, USER_NAME = "Can", USER_SURNAME = "Sever", USER_MAIL = "can.sever@bilgiyon.com.tr" };
+            result = new UserController().UpdateUser(user);
+            Assert.AreEqual(user.USER_NAME + " Kullanıcısı için Güncelleme Başarılı!", result);
+        }
+    }
+}
